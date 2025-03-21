c02ee8084c41be9e26d88fd56ba82efbc26e7eb3
Brian Hackworth <brian@hackworth.org>
2025-03-21 08:28:32 -0700

    arrange in the hacs format

---
+++ custom_components/hackware/__init__.py
@@ -0,0 +1,42 @@
+"""The sensorhub integration."""
+
+from __future__ import annotations
+
+import logging
+
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import CONF_PORT, EVENT_HOMEASSISTANT_STOP, Platform
+from homeassistant.core import HomeAssistant
+from homeassistant.exceptions import ConfigEntryNotReady
+
+from .hub import HackHub
+
+_LOGGER = logging.getLogger(__name__)
+
+_PLATFORMS = [Platform.SENSOR]
+
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Set up the entries we know about."""
+
+    hub = HackHub(hass, entry.data[CONF_PORT], entry)
+    hub.start()
+    if not hub.is_started():
+        raise ConfigEntryNotReady("Can't start the Hackware hub")
+
+    entry.runtime_data = hub
+    cancel = hass.bus.async_listen_once(EVENT_HOMEASSISTANT_STOP, hub.stop)
+    cancel()
+
+    await hass.config_entries.async_forward_entry_setups(entry, _PLATFORMS)
+
+    return True
+
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Unload the configuration."""
+
+    hub = entry.runtime_data
+    hub.stop()
+
+    return await hass.config_entries.async_unload_platforms(entry, _PLATFORMS)
